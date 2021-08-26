0f20cfb3a8bcaf1212805a03ddf41b8ad584c834
Craig Neff <ifly12610@gmail.com>
2021-08-25 23:04:02 -0600

    added the local storage and function to reset it. Good practice of oop and all 4 pillars -- Encapsulation, Abstraction, Polymorphism, Inheritance.

--- script.js
+++ script.js
@@ -71,7 +71,11 @@ class App {
   #mapEvent;
   #workouts = [];
   constructor() {
+    //Get user position
     this._getPosition();
+    //get data from local storage
+    this._getLocalStorage();
+    //Attach event handlers
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
     containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
@@ -94,10 +98,9 @@ class App {
     const { longitude } = position.coords;
 
     const coords = [latitude, longitude];
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
     this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
-    console.log(this);
+    // console.log(this);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
@@ -105,6 +108,9 @@ class App {
     }).addTo(this.#map);
     //Handling clicks on Map
     this.#map.on('click', this._showForm.bind(this));
+    this.#workouts.forEach(work => {
+      this._renderWorkoutMarker(work);
+    });
   }
 
   _showForm(mapE) {
@@ -163,14 +169,16 @@ class App {
     }
     //Add new object to workout array
     this.#workouts.push(workout);
-    console.log(workout);
+
     //Render workout on map as marker
     this._renderWorkoutMarker(workout);
     //Render workout on list
     this._renderWorkout(workout);
-    console.log(this);
+
     //Clear INput Fields
     this._hideForm();
+    //Set local storage to all workouts
+    this._setLocalStorage();
   }
   _renderWorkoutMarker(workout) {
     L.marker(workout.cords)
@@ -188,7 +196,6 @@ class App {
         `${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♂️'} ${workout.description}`
       )
       .openPopup();
-    console.log(workout.type);
   }
 
   _renderWorkout(workout) {
@@ -242,12 +249,12 @@ class App {
 
   _moveToPopup(e) {
     const workoutEl = e.target.closest('.workout');
-    console.log(workoutEl);
+
     if (!workoutEl) return;
     const workout = this.#workouts.find(
       work => work.id === workoutEl.dataset.id
     );
-    console.log(workout);
+
     this.#map.setView(workout.cords, this.#mapZoomLevel, {
       animate: true,
       pan: {
@@ -258,5 +265,24 @@ class App {
     //Using the public interface:
     workout.click();
   }
+  _setLocalStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('workouts'));
+
+    if (!data) return;
+
+    this.#workouts = data;
+
+    this.#workouts.forEach(work => this._renderWorkout(work));
+  }
+
+  reset() {
+    localStorage.removeItem('workouts');
+    location.reload();
+  }
 }
 const app = new App();
+console.log(`app.reset() in console to reset local storage`);
